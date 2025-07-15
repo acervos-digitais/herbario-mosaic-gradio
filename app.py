ed54d78ab1bdf0a33caf5c6cea173471cdd9df6e
Thiago Hersan <thiago.hersan+github@gmail.com>
2025-07-15 17:22:19 -0400

    use pre-cropped images for mosaic

--- app.py
+++ app.py
@@ -12,21 +12,23 @@ from utils import boxpct2pix, centerpct2boxpix
 
 OBJS_URLS = "https://raw.githubusercontent.com/acervos-digitais/herbario-data/main/json/20250705_processed.json"
 IMG_URL = "https://digitais.acervos.at.eu.org/imgs/herbario/arts"
-IMG_DIR = "./imgs/full"
+IMG_CROPS_DIR = "./imgs/crops"
+IMG_FULL_DIR = "./imgs/full"
+GRID_MIN_CROP_HEIGHT = 64
 XY_OUT_DIM = (1024, 1024)
 XY_CROP_MAX = 0.45
 MAX_PIXELS = 2**25
 PImage.MAX_IMAGE_PIXELS = 2 * MAX_PIXELS
 
 
-def get_min_height_and_size(idObjIdxs_data, min_min_height=64):
+def get_min_height_and_size(idObjIdxs_data, min_min_height=GRID_MIN_CROP_HEIGHT):
   heights = []
   sizes = {}
 
   for idObjIdxs in idObjIdxs_data:
     id = idObjIdxs["id"]
 
-    img = PImage.open(path.join(IMG_DIR, f"{id}.jpg"))
+    img = PImage.open(path.join(IMG_FULL_DIR, f"{id}.jpg"))
     iw,ih = img.size
     sizes[id] = (iw,ih)
 
@@ -70,10 +72,6 @@ def get_mosaic_size(idObjIdxs_data, height_min, sizes):
 def get_grid_mosaic(idObjIdxs_all):
   idObjIdxs_data = [x for x in idObjIdxs_all if len(x["objIdxs"]) > 0]
 
-  for id in [x["id"] for x in idObjIdxs_data]:
-    if not path.isfile(path.join(IMG_DIR, f"{id}.jpg")):
-      download_image(IMG_URL, id)
-
   height_min, sizes = get_min_height_and_size(idObjIdxs_data)
   mos_w, mos_h, limit_scale = get_mosaic_size(idObjIdxs_data, height_min, sizes)
 
@@ -83,18 +81,15 @@ def get_grid_mosaic(idObjIdxs_all):
   for idObjIdxs in idObjIdxs_data:
     id = idObjIdxs["id"]
 
-    img = PImage.open(path.join(IMG_DIR, f"{id}.jpg"))
-    iw,ih = img.size
-
     for idx in idObjIdxs["objIdxs"]:
-      (x0,y0,x1,y1) = all_data[id]["objects"][idx]["box"]
-      crop_w = iw * (x1 - x0)
-      crop_h = ih * (y1 - y0)
+      idx_str = f"0000{idx}"[-4:]
+      cimg = PImage.open(path.join(IMG_CROPS_DIR, f"{id}_{idx_str}.jpg"))
+      crop_w, crop_h = cimg.size
 
       scale_factor = limit_scale * (height_min / crop_h)
       crop_w, crop_h = int(scale_factor * crop_w), int(scale_factor * crop_h)
 
-      crop_img = img.crop((int(x0 * iw), int(y0 * ih), int(x1 * iw), int(y1 * ih))).resize((crop_w, crop_h))
+      crop_img = cimg.resize((crop_w, crop_h))
 
       if cur_y >= mos_h:
         print("break")
@@ -124,17 +119,13 @@ def get_grid_mosaic(idObjIdxs_all):
 def get_xy_mosaic(idObjIdxs_all):
   idObjIdxs_data = [x for x in idObjIdxs_all if len(x["objIdxs"]) > 0]
 
-  for id in [x["id"] for x in idObjIdxs_data]:
-    if not path.isfile(path.join(IMG_DIR, f"{id}.jpg")):
-      download_image(id)
-
   pix_cnts = np.zeros(XY_OUT_DIM)
   pix_vals = np.zeros((*XY_OUT_DIM, 3))
 
   for idObjIdxs in idObjIdxs_data:
     id = idObjIdxs["id"]
 
-    img = PImage.open(path.join(IMG_DIR, f"{id}.jpg")).convert("RGB")
+    img = PImage.open(path.join(IMG_FULL_DIR, f"{id}.jpg")).convert("RGB")
     iw,ih = img.size
     w_scale, h_scale = XY_OUT_DIM[0] / iw, XY_OUT_DIM[1] / ih
     crop_scale = min(w_scale, h_scale)
@@ -173,10 +164,13 @@ def get_xy_mosaic(idObjIdxs_all):
 
 
 ### prep files and dirs
-makedirs(IMG_DIR, exist_ok=True)
-objs_file_path = download_file(OBJS_URLS)
+makedirs(IMG_CROPS_DIR, exist_ok=True)
+makedirs(IMG_FULL_DIR, exist_ok=True)
+
+download_extract(f"{IMG_URL}/crops.tgz", "./imgs")
 download_extract(f"{IMG_URL}/full.tgz", "./imgs")
 
+objs_file_path = download_file(OBJS_URLS)
 with open(objs_file_path, "r") as ifp:
   all_data = json.load(ifp)
 
@@ -185,6 +179,7 @@ with open(objs_file_path, "r") as ifp:
 with gr.Blocks() as demo:
   gr.Interface(
     title="grid",
+    api_name="grid",
     fn=get_grid_mosaic,
     inputs="json",
     outputs="image",
@@ -193,6 +188,7 @@ with gr.Blocks() as demo:
 
   gr.Interface(
     title="xy",
+    api_name="xy",
     fn=get_xy_mosaic,
     inputs="json",
     outputs="image",
@@ -200,4 +196,4 @@ with gr.Blocks() as demo:
   )
 
 if __name__ == "__main__":
-   demo.launch()
+  demo.launch()
